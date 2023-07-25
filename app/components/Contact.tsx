3270bc7f11f76abbc5245ddc157e6561655e99ab
ducksblock <pranavjoshi71619@gmail.com>
2023-07-25 12:24:26 +0530

    Updated contact page links

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,87 +1,94 @@
 'use client'
+import Link from 'next/link'
 import { IoMail, IoLogoLinkedin, IoLogoTwitter } from 'react-icons/io5'
 
 const Contact = () => {
   return (
     <>
-      <div
-        className="
-          scroll-smooth
-          text-[40px]
-          md:text-[60px]
-          py-2
-          pl-6
-          md:pl-14
-          bg-black
-          text-white
-          hover:bg-white
-          hover:pl-10
-          hover:md:pl-20
-          hover:text-black
-          cursor-pointer
-          btn-effect
-          border-t-2
-          border-neutral-700
-          flex
-          justify-between
-          items-center
-        "
-      >
-        Email
-        <IoMail className="mr-6 md:mr-14" />
-      </div>
-      <div
-        className="
-          scroll-smooth
-          text-[40px]
-          md:text-[60px]
-          py-2
-          pl-6
-          md:pl-14
-          bg-black
-          text-white
-          hover:bg-white
-          hover:pl-10
-          hover:md:pl-20
-          hover:text-black
-          cursor-pointer
-          btn-effect
-          border-t-2
-          border-neutral-700
-          flex
-          justify-between
-          items-center
-        "
-      >
-        LinkedIn
-        <IoLogoLinkedin className="mr-6 md:mr-14" />
-      </div>
-      <div
-        className="
-          scroll-smooth
-          text-[40px]
-          md:text-[60px]
-          py-2
-          pl-6
-          md:pl-14
-          bg-black
-          text-white
-          hover:bg-white
-          hover:pl-10
-          hover:md:pl-20
-          hover:text-black
-          cursor-pointer
-          btn-effect
-          border-t-2
-          border-neutral-700
-          flex
-          justify-between
-          items-center
-        "
-      >
-        Twitter
-        <IoLogoTwitter className="mr-6 md:mr-14" />
-      </div>
+      <a href="mailto:abc@gmail.com">
+        <div
+          className="
+            scroll-smooth
+            text-[40px]
+            md:text-[60px]
+            py-2
+            pl-6
+            md:pl-14
+            bg-black
+            text-white
+            hover:bg-white
+            hover:pl-10
+            hover:md:pl-20
+            hover:text-black
+            cursor-pointer
+            btn-effect
+            border-t-2
+            border-neutral-700
+            flex
+            justify-between
+            items-center
+          "
+          >
+            Email
+            <IoMail className="mr-6 md:mr-14" />
+          </div>
+        </a>
+      <Link href="https://www.linkedin.com/company/carbonblocks" target="https://www.linkedin.com/company/carbonblocks">
+        <div
+          className="
+            scroll-smooth
+            text-[40px]
+            md:text-[60px]
+            py-2
+            pl-6
+            md:pl-14
+            bg-black
+            text-white
+            hover:bg-white
+            hover:pl-10
+            hover:md:pl-20
+            hover:text-black
+            cursor-pointer
+            btn-effect
+            border-t-2
+            border-neutral-700
+            flex
+            justify-between
+            items-center
+          "
+          >
+          LinkedIn
+          <IoLogoLinkedin className="mr-6 md:mr-14" />
+        </div>
+      </Link>
+      <Link href="https://twitter.com/0xcarbonblocks" target="https://twitter.com/0xcarbonblocks">
+        <div
+          className="
+            scroll-smooth
+            text-[40px]
+            md:text-[60px]
+            py-2
+            pl-6
+            md:pl-14
+            bg-black
+            text-white
+            hover:bg-white
+            hover:pl-10
+            hover:md:pl-20
+            hover:text-black
+            cursor-pointer
+            btn-effect
+            border-t-2
+            border-neutral-700
+            flex
+            justify-between
+            items-center
+          "
+        >
+          Twitter
+          <IoLogoTwitter className="mr-6 md:mr-14" />
+        </div>
+      </Link>
     </>
   )
 }
